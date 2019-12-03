ee12197b1f210ca0126053644a6f5d1dac83cebd
WalkerLiuFei <l592965@163.com>
2019-12-03 14:30:55 +0800

    Init with simple server.

---
+++ simaple-server/Readme.md
@@ -0,0 +1,7 @@
+# build small golang binary image.
+
+reference to [build smallest golang binary image](https://medium.com/@chemidy/create-the-smallest-and-secured-golang-docker-image-based-on-scratch-4752223b7324)
+
+## build images with multistage 
+  
+This is a sample server demo, use docker multistage to build a plain image.
---
+++ simaple-server/main.go
@@ -0,0 +1,40 @@
+package simaple_server
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"net"
+)
+
+func accept(listener net.Listener) {
+	for true {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err.Error())
+		}
+		go handleConn(conn)
+	}
+}
+
+func handleConn(conn net.Conn) {
+	fmt.Printf("Connect to %s \n", conn.RemoteAddr())
+	defer conn.Close()
+	reader := bufio.NewReader(conn)
+	for true {
+		data, err := reader.ReadBytes('\n')
+		if err != nil {
+			fmt.Printf("Error read data ：%s", err.Error())
+			conn.Close()
+		}
+		fmt.Printf("from client : %s ", string(data))
+		io.WriteString(conn, "Received\n")
+	}
+}
+func main() {
+	listener, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		panic(err)
+	}
+	accept(listener)
+}
