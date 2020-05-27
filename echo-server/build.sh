2a4e543e52e856590cb861c695180b293e97106b
刘飞 <walkerliu1993@gmail.com>
2020-05-27 17:23:16 +0800

    add comments

--- echo-server/build.sh
+++ echo-server/build.sh
@@ -12,9 +12,14 @@ echo  "--------------run dokcer image in kubernetes，https://kubernetes.io/docs
 
 echo "kubectl start the pod runing echo-server in docker and kubernetes cluster"
 kubectl apply -f ./deploy.yaml
+
 echo "expose a port through with a service 'echo-server-service'"
 kubectl expose deployment echo-server-deployment --type=LoadBalancer --name=echo-server-service
+
+# 这个时候才会将 端口暴露出来，但是不是通过 docker那样直接expose的端口，具体的端口需要从下面命令看到，
+# 具体的端口是在NodePort 那个属性值
+
 echo "describe depoloyment"
-kubectl describe pod "echo-server"
+kubectl describe service echo-server-service
 
 ## 在 mac 和 windows
